6c07ae04d17e156cc7c7b396f927ad7e40b05404
kulbir2423 <kulbir.singh1996@gmail.com>
2025-04-10 14:51:57 +0530

    Final Completion

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ let boxes = document.querySelectorAll(".box");
 let restBtn = document.querySelector(".reset");
 let btn = document.querySelector(".box");
 let turn0 = true;
+let startBtn= document.querySelector(".start-new");
+let winningMess = document.querySelector("#winning-message");
+let mess= document.querySelector(".message");
+
+
 
 const winPattterns = [
   [0, 1, 2],
   [0, 3, 6],
   [0, 4, 8],
-  [1,4,7],
+  [1, 4, 7],
   [2, 5, 8],
   [2, 4, 6],
   [3, 4, 5],
@@ -15,26 +20,67 @@ const winPattterns = [
 ];
 
 boxes.forEach((box) => {
-    box.addEventListener("click", ()=> {
-        console.log("box was clicked");
-        if(turn0===true) {
-            box.innerText = "0";
-            turn0= false;
-        }
-        else {
-            box.innerText = "X";
-            turn0 = true;
-        }
-        box.disabled = true;
+  box.addEventListener("click", () => {
+    console.log("box was clicked");
+    if (turn0 === true) {
+      box.innerText = "0";
+      turn0 = false;
+    } else {
+      box.innerText = "X";
+      turn0 = true;
+    }
+    box.disabled = true;
 
-        // Function to check
+    // Function to check
 
-        checkWinner();
-    })
-})
+    checkWinner();
+  });
+});
 
 const checkWinner = () => {
-    for(let pattern of winPattterns) {
-        console.log(pattern[0],pattern[1],pattern[2]);
+  for (let pattern of winPattterns) {
+    let pos1 = boxes[pattern[0]].innerText;
+    let pos2 = boxes[pattern[1]].innerText;
+    let pos3 = boxes[pattern[2]].innerText;
+
+    if(pos1 !="" && pos2 !="" && pos3!="") {
+        if(pos1 === pos2 && pos2 === pos3) {
+            console.log("Winner",pos2);
+            disableBoxes();
+            showWinner(pos2);
+        }
+    }
+  }
+};
+const showWinner = (winner) => {
+    winningMess.innerText = `Congratulation! Winner is ${winner}`;
+    mess.classList.remove("hide");
+
+}
+
+const resetGame = () => {
+    turn0 = true;
+    enableBoxes();
+    mess.classList.add("hide");
+}
+
+const disableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = true;
     }
 }
+
+const enableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+restBtn.addEventListener("click", () => {
+    resetGame();
+})
+
+startBtn.addEventListener("click",() => {
+    resetGame();
+})
